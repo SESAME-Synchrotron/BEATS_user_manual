608591e26a449329b58dcbd6fd2a367a2ea1d40a
gianthk <gianthk.iori@gmail.com>
2025-11-11 15:19:08 +0100

    minor

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -136,12 +136,12 @@ jupyter_execute_notebooks = "off"
 html_theme = 'default'
 
 # on_rtd is whether we are on readthedocs.org
-# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-# if not on_rtd:  # only import and set the theme if we're building docs locally
-# import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally
+  import sphinx_rtd_theme
+  html_theme = 'sphinx_rtd_theme'
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
